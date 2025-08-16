de9dcb8c8698afd8052d4233c80185163f25548c
Big Horny <bighorny@titties.local>
2025-08-17 02:48:06 +0300

    refactor ip

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "dickord/hub_client"
+import (
+	"dickord/hub_client"
+	"flag"
+)
+
+var addr = flag.String("addr", "ws://localhost:1488/ws", "server websocket addres")
 
 func main() {
-	hub_client.RunClient()
+	flag.Parse()
+	hub_client.RunClient(*addr)
 }
--- hub_client/client.go
+++ hub_client/client.go
@@ -1,4 +1,4 @@
-package hub
+package hub_client
 
 import (
 	"fmt"
